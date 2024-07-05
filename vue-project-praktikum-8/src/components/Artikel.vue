164070d4821a215ed2c3ced2d8ee6460b8a8379f
RAIHAN <raihan.alyzaky@gmail.com>
2024-07-05 18:12:40 +0700

    pertemuan8

---
+++ vue-project-praktikum-8/src/components/Artikel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <h1>Ini Artikel Page</h1>
+        <br>
+        <h3>Judul Artikel</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ex sapiente, deleniti sint suscipit architecto porro quod voluptatibus impedit hic nemo provident dolores voluptates expedita accusamus nostrum. Non, obcaecati autem?</p>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="../assets/ayam.gif" alt="" width="300px">
+        </div>
+        <div class="col-4">
+
+        </div>
+        <div class="col-4">
+
+        </div>
+    </div>
+</template>
---
+++ vue-project-praktikum-8/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Artikel from '../components/Artikel.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <Artikel />
+  </main>
+</template>
